9f808de21647d18bd4b9095b7c8c65846ff0e264
unknown <joshuaolagorioye@gmail.com>
2023-07-28 08:01:02 -0700

    bootstrap cdn added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import SkillLevelDisplay from './components/SkillLevelDisplay';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Card, {SampleText}from './components/Card';
 import Countdowntimer from './components/UI/Countdowntimer';
 export const App = () => {
